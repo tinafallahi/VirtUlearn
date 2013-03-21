33ed067f513fdaa63cf5172c66815ad440bf2241
Tina Fallahi <tina.fallahi.11@ucl.ac.uk>
2013-03-21 16:47:33 +0000

    Storage Added

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var http = require("http");
+var azure = require('azure');
+
+var storageAccount = "virtulearnstudents";
+var accessKey = "AacyAmuNenRszqPy9i3RnfSsYnJ3FKqXMfl+Qft1qMp5Vuy1YTTenIVX2D/op+jhXrEDrAv+wwbR8ckFeT/EwQ==";
+var containerName = "YAY";
 
 http.createServer(function (req, res) {
                     res.writeHead(200, {"Content-Type": "text/plain"});
                     res.end("Hello Node.js and Windows Azure Website!\n");
+                  
+                    var blobService = azure.createBlobService(storageAccount,accessKey);
+                    blobService.createContainerIfNotExists(containerName, function(error){
+                                  if(!error){
+                                  // Container exists and is private
+                        }
+                    });
+                    
                     }).listen(process.env.port);
  
 console.log("Server started.");
